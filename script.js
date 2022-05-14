dc142b0f5ffa16a20cb6cc4fb1608ef1803b2bcc
Herbert-Haase <Acedia_the_Skelektronik@gmx.de>
2022-05-14 17:43:33 +0200

    make cards appear, switch read/unread, delete cards

---
+++ script.js
@@ -0,0 +1,76 @@
+const note_down = document.querySelector(".sticky button");
+
+// const myLibrary = [];
+
+
+note_down.addEventListener("click", () => {
+        const title = document.querySelector("[name='title']");
+        const author = document.querySelector("[name='author']");
+        const pages = document.querySelector("[name='pages']");
+        const read = document.querySelector("[name='read']");
+
+        function Book(title, author, pages) {
+            this.title = title;
+            this.author = author;
+            this.pages = pages;
+            this.read = read;
+        }
+
+        const bookTitle = new Book(title.value, author.value, pages.value, read.checked);
+        // bookTitle.id = (() => myLibrary.length)();
+        // myLibrary.push(bookTitle);
+
+        (() => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+        
+        const title = document.createElement("p");
+        title.classList.add("title"); 
+        title.textContent = bookTitle.title;
+        card.appendChild(title);
+
+        const author = document.createElement("p");
+        author.classList.add("author"); 
+        author.textContent = bookTitle.author;
+        card.appendChild(author);
+
+        const hr = document.createElement("hr");
+        card.appendChild(hr);
+
+        const pages = document.createElement("p");
+        pages.classList.add("pages"); 
+        pages.textContent = `${bookTitle.pages} pages`;
+        card.appendChild(pages);
+
+        const read = document.createElement("button");
+        if(!bookTitle.read.checked) read.classList.add("unread"); 
+        read.classList.add("read");
+        card.appendChild(read);
+
+        const remove = document.createElement("button");
+        remove.classList.add("remove"); 
+        remove.textContent = "REMOVE";
+        card.appendChild(remove);
+
+        const main = document.querySelector("main");
+        main.appendChild(card);
+
+
+
+        
+    })()
+    const main = document.querySelector("main");
+    const card = main.querySelector(".card:last-child");
+    const readbtn = card.querySelector(".read");
+    const remove = card.querySelector(".remove");
+
+
+readbtn.addEventListener("click", () => {
+    readbtn.classList.toggle("unread");
+}); 
+
+remove.addEventListener("click", () => {
+    card.remove();
+})
+
+});
